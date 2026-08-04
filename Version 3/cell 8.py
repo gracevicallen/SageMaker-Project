38d9c2820941f18b109d9ca73e64e54e3b114cbc
Grace Allen <grace.allen@bauercomp.com>
2026-08-04 13:58:14 -0400

    Update cell 8.py

--- Version 3/cell 8.py
+++ Version 3/cell 8.py
@@ -1,1 +1,9 @@
+# OPTIONAL CALCULATE MAE
+average_absolute_error = (
+    results["Absolute Error"].mean()
+)
 
+print(
+    "Version 3 Rolling Average Absolute Error: "
+    f"{average_absolute_error:.2f} units"
+)
