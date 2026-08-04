f1e958b4d0bd4d591567d2f1c8289ebe35817089
Grace Allen <grace.allen@bauercomp.com>
2026-08-04 11:53:32 -0400

    Update cell 7.py

--- Version 2/cell 7.py
+++ Version 2/cell 7.py
@@ -1,1 +1,42 @@
+# DISPLAY PREDICTIONS WITH ACTUAL USAGE
+results = (
+    pd.concat(
+        all_results,
+        ignore_index=True,
+    )
+    .sort_values(
+        [
+            PART_COLUMN,
+            DATE_COLUMN,
+        ]
+    )
+    .reset_index(drop=True)
+)
 
+results["Predicted Usage Rounded"] = (
+    results["Predicted Usage"]
+    .round()
+    .astype(int)
+)
+
+results["Actual Usage Rounded"] = (
+    results["Actual Usage"]
+    .round()
+    .astype(int)
+)
+
+display(
+    results[
+        [
+            PART_COLUMN,
+            DATE_COLUMN,
+            "Training Through",
+            "Predicted Usage Rounded",
+            "Actual Usage Rounded",
+            "Predicted Usage",
+            "Actual Usage",
+            "Error",
+            "Absolute Error",
+        ]
+    ]
+)
