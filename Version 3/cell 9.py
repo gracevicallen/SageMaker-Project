266d64caa9edd1a362e91952436aa0af8c1f5299
Grace Allen <grace.allen@bauercomp.com>
2026-08-04 13:58:37 -0400

    Update cell 9.py

--- Version 3/cell 9.py
+++ Version 3/cell 9.py
@@ -1,1 +1,38 @@
+# ACCURACY BY PART
+part_accuracy = (
+    results
+    .groupby(PART_COLUMN)
+    .agg(
+        Average_Actual_Usage=(
+            "Actual Usage",
+            "mean",
+        ),
+        Average_Predicted_Usage=(
+            "Predicted Usage",
+            "mean",
+        ),
+        Actual_Total=(
+            "Actual Usage",
+            "sum",
+        ),
+        Predicted_Total=(
+            "Predicted Usage",
+            "sum",
+        ),
+        MAE=(
+            "Absolute Error",
+            "mean",
+        ),
+    )
+    .reset_index()
+)
 
+part_accuracy[
+    "Six_Month_Total_Error"
+] = (
+    part_accuracy["Predicted_Total"]
+    -
+    part_accuracy["Actual_Total"]
+)
+
+display(part_accuracy)
